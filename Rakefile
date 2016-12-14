bb26010ec06e8f986c8a032a81b961f18e7102d1
Keaton Burleson <keaton.burleson@me.com>
2016-12-14 13:59:49 -0600

    Added a config function

--- Rakefile
+++ Rakefile
@@ -3,4 +3,5 @@ require 'unirest'
 require 'barby'
 require 'barby/barcode/code_128'
 require 'barby/outputter/html_outputter'
+require 'yaml'
 Bundler::GemHelper.install_tasks
--- lib/aura-print.rb
+++ lib/aura-print.rb
@@ -1,10 +1,40 @@
+require 'yaml'
 module AuraPrint
+    @config = {
+        print_address: 'http://10.0.2.232/printer/aura.php',
+        printer: 'Stage1'
+    }
+
+    @valid_config_keys = @config.keys
+
+    # Configure through hash
+    def self.configure(opts = {})
+        opts.each { |k, v| @config[k.to_sym] = v if @valid_config_keys.include? k.to_sym }
+    end
+
+    # Configure through yaml file
+    def self.configure_with(path_to_yaml_file)
+        begin
+            config = YAML.load(IO.read(path_to_yaml_file))
+        rescue Errno::ENOENT
+            puts "YAML configuration file couldn't be found. Using defaults."; return
+        rescue Psych::SyntaxError
+            puts 'YAML configuration file contains invalid syntax. Using defaults.'; return
+        end
+
+        configure(config)
+    end
+
+    def self.config
+        @config
+    end
+
     def self.barcodeWeb(sku)
         require 'unirest'
         Unirest.post('http://10.0.2.232/printer/aura.php',
                      parameters: { sku: sku, printer: 'Stage1' })
 
-        return "Success"
+        'Success'
     end
 
     def self.systemPrint(sku)
@@ -13,6 +43,6 @@ module AuraPrint
         require 'barby/outputter/html_outputter'
 
         barcode = Barby::Code128B.new(sku)
-        return barcode.to_html
+        barcode.to_html
     end
 end
